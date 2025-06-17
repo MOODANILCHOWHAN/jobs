98a2717fc0be6e0d005b7ee67a6b4623356df53b
MOODANILCHOWHAN <anilchowhan1111@gmail.com>
2025-06-17 16:40:17 +0530

    testing

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
-// run `node index.js` in the terminal
+// // run `node index.js` in the terminal
 
-// console.log(`Hello Node.js v${process.versions.node}!`);
+// // console.log(`Hello Node.js v${process.versions.node}!`);
 
-import express, { json } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import router from './routes/jobRoute.js';
+// import express, { json } from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+// import router from './routes/jobRoute.js';
+// dotenv.config();
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // console.log('MONGO_URI:', process.env.MONGO_URI);
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDb connected successfully"))
+//   .catch((error) => console.log("Failed to connect mongoDB", error));
+//   app.use('/',router)
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log('server was running at ports',port);
+// });
+
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import router from "./routes/jobRoute.js";
+import cors from "cors";
+
 dotenv.config();
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// console.log('MONGO_URI:', process.env.MONGO_URI);
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json({ limit: "30mb" }));
+
+app.use("/", router);
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log(' MongoDB connected'))
-  .catch((err) => console.error(' MongoDB connection error:', err));
+  .then(() => console.log("MongoDb connected successfully"))
+  .catch((error) => console.log("Failed to connect mongoDB", error));
 
-  app.use('/',router)
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('server was running at ports',port);
-});
+app.listen(PORT, console.log(`server running ${PORT}`));
